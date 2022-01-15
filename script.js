c417fa4dfc6b5caff08b95f99a0b0b7d02f5e419
lcleandrocosta <lcleandrocosta@gmail.com>
2022-01-15 15:56:20 -0300

    Modificar as variáveis condicaoEsqerda, direita e central para statusEsquerda, central e direta

--- script.js
+++ script.js
@@ -2,19 +2,19 @@
 const imagemEsquerda = document.querySelector('#img-esquerda');
 const nomeDoPersonagemEsquerda = document.querySelector('#nome-esquerda');
 const especieEsquerda = document.querySelector('#especie-esquerda');
-const condicaoEsquerda = document.querySelector('#status-esquerda');
+const statusEsquerda = document.querySelector('#status-esquerda');
 
 //Declaração de variáveis dos elementos da coluna central
 const imagemCentral = document.querySelector('#img-central');
 const nomeDoPersonagemCentral = document.querySelector('#nome-central');
 const especieCentral = document.querySelector('#especie-central');
-const condicaoCentral = document.querySelector('#status-central');
+const statusCentral = document.querySelector('#status-central');
 
 //Declaração de variáveis dos elementos da coluna direita
 const imagemDireita = document.querySelector('#img-direita');
 const nomeDoPersonagemDireita = document.querySelector('#nome-direita');
 const especieDireita = document.querySelector('#especie-direita');
-const condicaoDireita = document.querySelector('#status-direita');
+const statusDireita = document.querySelector('#status-direita');
 
 //Declaração da variável do Botão
 const botao = document.querySelector('button');
