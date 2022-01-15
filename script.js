7fe27254455501773f4b9751e14f4d4262d8e272
lcleandrocosta <lcleandrocosta@gmail.com>
2022-01-15 15:37:49 -0300

    Declara e atribuir variáveis da coluna central do index.html

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 //Declaração de variáveis dos elementos da coluna esquerda
 const imagemEsquerda = document.querySelector('#img-esquerda');
-const nomeDoPersonagemEsquerda = document.querySelector('#nome');
-const especieEsquerda = document.querySelector('#especie');
-const condicaoEsquerda = document.querySelector('#status');
+const nomeDoPersonagemEsquerda = document.querySelector('#nome-esquerda');
+const especieEsquerda = document.querySelector('#especie-esquerda');
+const condicaoEsquerda = document.querySelector('#status-esquerda');
+
+//Declaração de variáveis dos elementos da coluna central
+const imagemCentral = document.querySelector('#img-central');
+const nomeDoPersonagemCentral = document.querySelector('#nome-central');
+const especieCentral = document.querySelector('#especie-central');
+const condicaoCentral = document.querySelector('#status-central');
 
 //Declaração da variável do Botão
 const botao = document.querySelector('button');
