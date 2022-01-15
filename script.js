6ab7c4e534bfe56ba13330de290bfe023cbc67b3
lcleandrocosta <lcleandrocosta@gmail.com>
2022-01-15 15:30:38 -0300

    Declara e atribuir variáveis da coluna esquerda do index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+//Declaração de variáveis dos elementos da coluna esquerda
+const imagemEsquerda = document.querySelector('#img-esquerda');
+const nomeDoPersonagemEsquerda = document.querySelector('#nome');
+const especieEsquerda = document.querySelector('#especie');
+const condicaoEsquerda = document.querySelector('#status');
+
+//Declaração da variável do Botão
+const botao = document.querySelector('button');
